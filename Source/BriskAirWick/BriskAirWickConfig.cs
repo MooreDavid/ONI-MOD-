dc5ba6f11aaa52b7742029092ac883d82597358b
MooreDavid <ziggydfsm@gmail.com>
2018-10-02 20:59:48 -0400

    New Mod

---
+++ Source/BriskAirWick/BriskAirWickConfig.cs
@@ -0,0 +1,77 @@
+﻿using STRINGS;
+using System.Collections.Generic;
+using TUNING;
+using UnityEngine;
+
+public class BriskAirWickConfig : IBuildingConfig
+
+    
+{
+    public const string ID = "BRISKAIRWICK";
+
+    public override BuildingDef CreateBuildingDef()
+    {
+        string id = "BRISKAIRWICK";
+        int width = 1;
+        int height = 2;
+        string anim = "lava_lamp_kanim";
+        int hitpoints = 30;
+        float construction_time = 30f;
+        float[] tieR2 = TUNING.BUILDINGS.CONSTRUCTION_MASS_KG.TIER2;
+        string[] rawMetals = MATERIALS.RAW_METALS;
+        float melting_point = 1600f;
+        BuildLocationRule build_location_rule = BuildLocationRule.OnFloor;
+        EffectorValues tieR0 = NOISE_POLLUTION.NOISY.TIER0;
+        BuildingDef buildingDef = BuildingTemplates.CreateBuildingDef(id, width, height, anim, hitpoints, construction_time, tieR2, rawMetals, melting_point, build_location_rule, new EffectorValues { amount = 2, radius = 5 }, tieR0, 0.2f);
+        buildingDef.InputConduitType = ConduitType.Liquid;
+        buildingDef.RequiresPowerInput = true;
+        buildingDef.EnergyConsumptionWhenActive = 2f;
+        buildingDef.ViewMode = SimViewMode.LiquidVentMap;
+        buildingDef.AudioCategory = "Metal";
+        buildingDef.UtilityInputOffset = new CellOffset(0, 0);
+        buildingDef.UtilityOutputOffset = new CellOffset(0, 0);
+        return buildingDef;
+    }
+
+    public override void ConfigureBuildingTemplate(GameObject go, Tag prefab_tag)
+    {
+        go.GetComponent<KPrefabID>().AddTag(GameTags.Decoration);
+        Storage storage1 = go.AddOrGet<Storage>();
+        storage1.showInUI = true;
+
+        ConduitConsumer conduitConsumer = go.AddOrGet<ConduitConsumer>();
+        conduitConsumer.conduitType = ConduitType.Liquid;
+        conduitConsumer.consumptionRate = 1.5f;
+        conduitConsumer.capacityTag = GameTagExtensions.Create(SimHashes.DirtyWater);
+        conduitConsumer.capacityKG = 10f;
+        conduitConsumer.forceAlwaysSatisfied = true;
+        conduitConsumer.wrongElementResult = ConduitConsumer.WrongElementResult.Dump;
+
+        ManualDeliveryKG manualDeliveryKg1 = go.AddOrGet<ManualDeliveryKG>();
+        manualDeliveryKg1.SetStorage(storage1);
+        manualDeliveryKg1.requestedItemTag = "BasicFabric";
+        manualDeliveryKg1.capacity = 10f;
+        manualDeliveryKg1.refillMass = 1f;
+        manualDeliveryKg1.choreTypeIDHash = Db.Get().ChoreTypes.OperateFetch.IdHash;
+        go.AddOrGet<WaterPurifier>();
+
+        ElementConverter elementConverter = go.AddComponent<ElementConverter>();
+        elementConverter.consumedElements = new ElementConverter.ConsumedElement[2]
+			{
+				new ElementConverter.ConsumedElement("BasicFabric", 0.000375f),
+				new ElementConverter.ConsumedElement(SimHashes.DirtyWater.CreateTag(), 0.02f)
+			};
+        elementConverter.outputElements = new ElementConverter.OutputElement[1]
+			{
+				new ElementConverter.OutputElement(0.02f, SimHashes.ContaminatedOxygen, 303.15f, false, 0.0f, 1f, true),
+			};
+
+        Prioritizable.AddRef(go);
+    }
+
+    public override void DoPostConfigureComplete(GameObject go)
+    {
+        BuildingTemplates.DoPostConfigure(go);
+        go.GetComponent<KPrefabID>().prefabInitFn += (KPrefabID.PrefabFn)(game_object => new PoweredActiveController.Instance((IStateMachineTarget)game_object.GetComponent<KPrefabID>()).StartSM());
+    }
+}
---
+++ Source/BriskAirWick/BriskAirWickMod.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Database;
+using Harmony;
+using KSerialization;
+using STRINGS;
+using BUILDINGS = TUNING.BUILDINGS;
+
+namespace BriskAirWick
+{
+    public class BriskAirWickMod
+    {
+        [HarmonyPatch(typeof(GeneratedBuildings), "LoadGeneratedBuildings")]
+        public class BriskAirWickBuildingsPatch
+        {
+            private static void Prefix()
+            {
+                Strings.Add("STRINGS.BUILDINGS.PREFABS.BRISKAIRWICK.NAME", "Air Wick");
+                Strings.Add("STRINGS.BUILDINGS.PREFABS.BRISKAIRWICK.DESC", "A Wick abosrbs the Polluted Water then releases Polluted Oxygen. Smell the Freshness");
+                Strings.Add("STRINGS.BUILDINGS.PREFABS.BRISKAIRWICK.EFFECT", "Pushes " + (string) ELEMENTS.CONTAMINATEDOXYGEN.NAME + " into " + (string) ELEMENTS.DIRTYWATER.NAME + "With use of" + (string) STRINGS.CREATURES.SPECIES.BASICFABRICMATERIALPLANT.NAME + ".");
+
+                List<string> oxygenBuildings = new List<string>((string[])BUILDINGS.PLANORDER[1].data) { BriskAirWickConfig.ID };
+                BUILDINGS.PLANORDER[1].data = oxygenBuildings.ToArray();
+                BUILDINGS.COMPONENT_DESCRIPTION_ORDER.Add(BriskAirWickConfig.ID);
+            }
+
+            private static void Postfix()
+            {
+                object obj = Activator.CreateInstance(typeof(BriskAirWickConfig));
+                BuildingConfigManager.Instance.RegisterBuilding(obj as IBuildingConfig);
+            }
+        }
+
+        [HarmonyPatch(typeof(Db), "Initialize")]
+        public class BriskAirWickDbPatch
+        {
+            private static void Prefix()
+            {
+                List<string> ls = new List<string>(Techs.TECH_GROUPING["ImprovedOxygen"]) { BriskAirWickConfig.ID };
+                Techs.TECH_GROUPING["ImprovedOxygen"] = ls.ToArray();
+            }
+        }
+
+    }
+}
